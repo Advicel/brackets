393be258f32e407cc7cd11a804b133518cf90cd0
unknown <sowetnikov2010@yandex.ru>
2020-03-11 16:47:23 +0300

    init: brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if(str.length%2==0){
+    let n = str.length;
+    let arr = new Array;
+    for(let i =0;i<bracketsConfig.length;i++){
+      arr.push(bracketsConfig[i].join(""));
+    }
+    for(let i =0;i<n/2;i++){
+      for(let j = 0;j<arr.length;j++){
+        str = str.split(arr[j]);
+        str=str.join("");
+      }
+    }
+    if(str) return false;
+    return true;
+  }
+  return false;
 }
